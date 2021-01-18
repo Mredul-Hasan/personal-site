68cb24acddcc5de8920849a8ea4c9e9af19bdd0e
mredul <mredulhasan93@gmail.com>
2021-01-18 21:27:55 +0600

    intro updated

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -12,21 +12,18 @@ const SideBar = () => (
         <img src={`${PUBLIC_URL}/images/me.jpg`} alt="" />
       </Link>
       <header>
-        <h2>Michael D&apos;Angelo</h2>
-        <p><a href="mailto:michael.l.dangelo@gmail.com">michael.l.dangelo@gmail.com</a></p>
+        <h2>Mredul Hasan</h2>
+        <p><a href="mailto:mredulhasan93@gmail.com">mredulhasan93@gmail.com</a></p>
       </header>
     </section>
 
     <section className="blurb">
       <h2>About</h2>
-      <p>Hi, I&apos;m Michael. I like building things.
-        I am a <a href="https://icme.stanford.edu/">Stanford ICME</a> graduate, YC Alumni, and
-        the co-founder and CTO of <a href="https://arthena.com">Arthena</a>. Before Arthena I was
-        at <a href="https://matroid.com">Matroid</a>
-        , <a href="https://planet.com">Planet</a>
-        , <a href="https://planetaryresources.com">Planetary Resources</a>
-        , <a href="https://facebook.com">Facebook</a>
-        , and <a href="https://seds.org">SEDS</a>.
+      <p>Hi, I&apos;m Mredul. I am a JS learner, MERN enthusiast.
+        I am a <a href="http://cseku.ac.bd/">KUCSE</a> graduate. Currently working on Reactjs
+        at <a href="http://techoptions.com.bd/">TechOptions</a>. Before here I worked at
+        , <a href="https://www.aamratechnologies.com/">Aamra Technologies Limited</a> as Programmer,
+        , <a href="#0">Rafiqul Islam College</a> as a Lecturer.
       </p>
       <ul className="actions">
         <li>
