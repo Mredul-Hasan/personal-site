1721fc4a7a2f447915d2530222eb157a9ba59ddd
mredul <mredulhasan93@gmail.com>
2021-01-18 22:50:57 +0600

    app header updated

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -5,8 +5,8 @@ import Main from '../layouts/Main';
 
 const Index = () => (
   <Main
-    description={"Michael D'Angelo's personal website. New York based Stanford ICME graduate, "
-    + 'co-founder and CTO of Arthena, and YC Alumni.'}
+    description={"Mredul Hasan's personal website. MERN enthusiast, "
+    + 'Jr. Software Engineer, Ex-Lecturer & KUCSE Alumni.'}
   >
     <article className="post" id="index">
       <header>
@@ -25,7 +25,7 @@ const Index = () => (
         view <Link to="/stats">site statistics</Link>, {' '}
         or <Link to="/contact">contact</Link> me.
       </p>
-      <p> Source available <a href="https://github.com/mldangelo/personal-site">here</a>.</p>
+      <p> Source available <a href="https://github.com/Mredul-Hasan/personal-site">here</a>.</p>
     </article>
   </Main>
 );
